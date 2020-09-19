8c3e2c56707847b0c8178e31aebb5d31fb679961
eileenyoo2 <eileenyoo2@gmail.com>
2020-09-19 23:07:46 +0900

    storyboard 재정리, HomeCustomCell 추가 (코드로만 구현해보기)

--- View/ViewController.swift
+++ View/ViewController.swift
@@ -10,36 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var homeTableView: UITableView!
-    
-    let arr = ["a","b","c"]
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        homeTableView.delegate = self
-        homeTableView.dataSource = self
-        homeTableView.register(UINib(nibName: "HomeTableViewCell1", bundle: nil), forCellReuseIdentifier: "HomeTableViewCell1")
-        
     }
-}
-
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "HomeTableViewCell1", for: indexPath) as! HomeTableViewCell1
-        
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 400
-    }
-    
-    
 }
 
